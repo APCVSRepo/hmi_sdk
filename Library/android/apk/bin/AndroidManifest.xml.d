7cd7b44f305b53b2684b1984fb0c1104f21111bf
xuwenlong <smartdevice475@163.com>
2015-12-02 17:05:55 +0800

    android apk

--- Library/android/apk/bin/AndroidManifest.xml.d
+++
@@ -1,2 +0,0 @@
-F:\work\git\Win32\build-AppLinkEmulater-Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_1-Release\android-build\bin\AndroidManifest.xml : \
-F:\work\git\Win32\build-AppLinkEmulater-Android_for_armeabi_v7a_GCC_4_8_Qt_5_4_1-Release\android-build\AndroidManifest.xml \
